32264d5b5d7e3382bea96e0e384af3ea7f26ce5c
lindyem <lindy.merwin@gmail.com>
2020-08-09 14:31:21 -0700

    Implement Filter

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -7,23 +7,52 @@ class EmployeeTable extends Component {
   state = {
     isLoaded: false,
     users: [],
+    filter: '',
+    sort: '',
+    allUsers: [],
   };
 
   componentDidMount() {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
-        console.log("response", response.data);
-        this.setState({ isLoaded: true, users: response.data });
+        this.setState({ isLoaded: true, users: response.data, allUsers: response.data });
       })
       .catch((err) => console.error(err));
   }
 
+  handleFilterChange = (event) => {
+    const { allUsers } = this.state;
+
+    const filterString = event.target.value;
+
+    const filteredUsers = allUsers.filter((user) => {
+      return user.name.includes(filterString);
+    })
+
+    this.setState({users: filteredUsers});
+  }
+
   render() {
-    if (this.state.isLoaded) {
+    const {
+      isLoaded,
+      users
+    } = this.state;
+
+    if (isLoaded) {
       return (
         <div>
           <h1>Employee Directory</h1>
+          <label htmlFor="filter">Filter: </label>
+          <input id="filter" name="filter" type="text" onChange={this.handleFilterChange}></input>
+
+          <label htmlFor="sort">Sort: </label>
+          <select name="sort" id="sort">
+            <option value="id">ID</option>
+            <option value="name">Name</option>
+            <option value="email">Email</option>
+            <option value="phone">Phone</option>
+          </select>
           <Table>
             <thead>
               <tr>
@@ -34,9 +63,15 @@ class EmployeeTable extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.users.map((user) => {
+              {users.map((user) => {
                 return (
-                  <TableRow id={user.id} name={user.name} email={user.email} phone={user.phone}/>
+                  <TableRow
+                    key={user.id}
+                    id={user.id}
+                    name={user.name}
+                    email={user.email}
+                    phone={user.phone}
+                  />
                 );
               })}
             </tbody>
