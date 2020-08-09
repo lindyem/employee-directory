0b409a0350e789374d5b8951f0f7171b9015dd88
lindyem <lindy.merwin@gmail.com>
2020-08-09 13:36:38 -0700

    Populate table and add loading screen

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -4,44 +4,57 @@ import { Table } from 'react-bootstrap';
 
 class EmployeeTable extends Component {
   state = {
+    isLoaded: false,
     users: []
   };
-  
+
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then((response) => {
         console.log('response', response.data);
-        this.setState({users: response.data})
+        this.setState({isLoaded:true, users: response.data})
       })
       .catch((err) => console.error(err));
   }
 
   render() {
-    console.log('users in render', this.state.users);
-    return (
-      <div>
-        <h1>Employee Directory</h1>
-        <Table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            
-          </tbody>
-        </Table>
-      </div>
-    );
+    if(this.state.isLoaded) {
+      return (
+        <div>
+          <h1>Employee Directory</h1>
+          <Table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.map((user) => {
+                return (
+                  <tr>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.phone}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Employee Directory</h1>
+          <div>Loading...</div>
+        </div>
+      )
+    }
+   
   }
 }
 
